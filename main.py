dcbfdae52ecd6d043f01ecbdbfc9c6c683c5008e
poridg <158010@ishweb.nl>
2025-06-22 16:27:25 +0200

    First steps

--- main.py
+++ main.py
@@ -1,18 +1,88 @@
 # imports n stuff
 import pygame
-import pygame.locals
+from pygame.locals import *
+import sys 
+import random
 
-def add(n1, n2)
-        return n1+n2
+FPS = 60
+FramePerSec = pygame.time.Clock()
 
-def main():
-    """
-    this runs pygame
-    """
-    pygame.init()
+#Colors 
+BLACK = (0, 0, 0)           # Black
+WHITE = (255, 255, 255)     # White
+GREY = (128, 128, 128)     # Grey
+RED = (255, 0, 0)         # Red\
+GREEN = (0, 255, 0)
+
+pygame.init()
+
+# Screen information
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+
+DISPLAYSURF = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Cars")
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/Enemy.png")
+        self.rect = self.image.get_rect()
+        self.rect.center=(random.randint(40,SCREEN_WIDTH-40),0)
+
+    def move(self):
+        self.rect.move_ip(0,10)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(30, 370), 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load("assets/Player.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+
+    def update(self):
+        pressed_keys = pygame.key.get_pressed()
+        #if pressed_keys[K_UP]:
+            #self.rect.move_ip(0, -5)
+       #if pressed_keys[K_DOWN]:
+            #self.rect.move_ip(0,5)
+
+        if self.rect.left > 0:
+            if pressed_keys[K_LEFT]:
+                self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:
+            if pressed_keys[K_RIGHT]:
+                self.rect.move_ip(5, 0)
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)        
+
+P1 = Player()
+E1 = Enemy()
 
 
-if __name__ == "__main__":
-    main()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    P1.update()
+    E1.move()
+
+    DISPLAYSURF.fill(WHITE)
+    P1.draw(DISPLAYSURF)
+    E1.draw(DISPLAYSURF)
+
+
+    pygame.display.update() 
+    FramePerSec.tick(FPS)
 
 
